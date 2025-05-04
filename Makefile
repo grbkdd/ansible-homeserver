730c5fb97044c4ba4b0c0511efee18d13daddeca
Marcin Grabda <marcin.grabda@gmail.com>
2025-05-04 18:11:55 -0500

    Replace requirements.sh with 'make deps'. Minor changes.

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-default: lint
+default: lint deps
 
 lint:
 	yamllint .
 	ansible-lint
+
+deps:
+	ansible-galaxy role install -r requirements.yml -p .galaxy-roles
+	ansible-galaxy collection install -r requirements.yml -p .galaxy-collections
--- requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing required roles..."
-ansible-galaxy role install -r requirements.yml -p .galaxy-roles
-echo "Installing required collections..."
-ansible-galaxy collection install -r requirements.yml -p .galaxy-collections
-echo "Done."
